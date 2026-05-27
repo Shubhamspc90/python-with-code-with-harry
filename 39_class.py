685bcfe9d93aac097fbd7516ede29f7d007f7548
Shubhamspc90 <shubham.spc90@gmail.com>
2026-05-27 13:59:11 +0530

    class in pyhton completed

--- 39_class.py
+++ 39_class.py
@@ -2,4 +2,17 @@
 # class is a blue print or template 
 # example:  form for an exam that contain  ,name, age , elective, father name etc
   
-# Object : Specific instance that is create from  template class 
+# Object : Specific instance that is create from  template class  .
+# example : a form that contain the data of shubham chauhan
+
+class Employee:
+    company="HP"
+    
+    def get_Salary(self): # self is important becoz self is the way to reference the object of the class which is being created 
+        return 43000
+
+
+e=Employee()
+print(e.get_Salary())
+e2=Employee()
+print(e2.company)
