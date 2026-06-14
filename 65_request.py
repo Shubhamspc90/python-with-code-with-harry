a07b756617e516137b1a8618d5bdfa6ef81a76ac
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-14 18:21:41 +0530

    small changes

--- 65_request.py
+++ 65_request.py
@@ -1,5 +1,9 @@
+# import requests
+# r = requests.get('https://api.github.com/events')
+# print(r.text)
+# with open("request.txt","w") as f:
+#     f.write(r.text)
+
 import requests
-r = requests.get('https://api.github.com/events')
-print(r.text)
-with open("request.txt","w") as f:
-    f.write(r.text)
+
+print(requests.__version__)
