7bfce47239e8e1539680d7760f2e6e6486e60a96
Shubhamspc90 <shubham.spc90@gmail.com>
2026-06-15 19:45:37 +0530

    added exit feature in calulator

--- calculator/main.py
+++ calculator/main.py
@@ -1,21 +1,28 @@
 try:
-    a=int(input("Enter 1st number: "))
-    b=int(input("Enter 2nd number: "))
-    print('''List of Operation you can perform:
-    press + for addition\npress - for substraction\npress * for multiplication\npress /for division''')
+    while(True):
+        choice=input(("press 'q' to quit or Press Enter to continue."))
+        if choice.lower()=='q':
+            print("Program Exited Successfully")
+            break
+        a=int(input("Enter 1st number: "))
+        b=int(input("Enter 2nd number: "))
+        print('''List of Operation you can perform:
+        press + for addition\npress - for substraction\npress * for multiplication\npress /for division''')
+        
+        o=input("enter operation: ")
+        match o:
+            case "+":
+                print(f"sum of {a} and {b} = {a+b}")
+            case "-":
+                print(f"sum of {a} and {b} = {a-b}")
+            case "*":
+                print(f"sum of {a} and {b} = {a*b}")
+            case "/":
+                print(f"sum of {a} and {b} = {a/b}")
+            case _:
+                print("wrong operator!")
     
-    o=input("enter operation: ")
-    match(o):
-        case "+":
-            print(f"sum of {a} and {b} = {a+b}")
-        case "-":
-            print(f"sum of {a} and {b} = {a-b}")
-        case "*":
-            print(f"sum of {a} and {b} = {a*b}")
-        case "/":
-            print(f"sum of {a} and {b} = {a/b}")
-        case default:
-            print("there is an error!")
-    
-except Exception  as e:
-    print("Enter valid no. a and b")
+except ValueError:
+    print("Enter valid number")
+except ZeroDivisionError:
+    print("Cannot divide by zero")
